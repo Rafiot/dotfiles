c46f5b2f918a6820944c66b54c8851ee35e7678f
Raphaël Vinot <raphael@vinot.info>
2018-08-02 21:58:12 +0200

    re add dep

--- init_env_ubuntu.sh
+++ init_env_ubuntu.sh
@@ -11,6 +11,7 @@ pip install --user --upgrade flake8 pew
 git rm -r --cached _vim/bundle/
 rm -rf _vim/bundle/*
 
+git submodule add --force https://github.com/SirVer/ultisnips.git _vim/bundle/ultisnips
 git submodule add --force https://github.com/tpope/vim-fugitive.git _vim/bundle/fugitive
 git submodule add --force https://github.com/scrooloose/syntastic.git _vim/bundle/syntastic
 git submodule add --force https://github.com/pgilad/vim-skeletons.git _vim/bundle/vim-skeletons
